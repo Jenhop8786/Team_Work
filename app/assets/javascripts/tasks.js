a1b167c5c01fabe8f243637c475c5876fba5b87b
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-10-14 08:39:31 -0700

    create tasks.js file

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,8 +1,44 @@
-// $(function() {
-//   $('#new_task').hide();
-//
-//   $('#add_task').click(function() {
-//     $('#new_task').show();
-//   });
-// });
-// }
+class Task {
+  constructor(data) {
+    this.userName = data.user.name
+    this.assignmentId = data.assignment_id
+    this.userId = data.user_id
+    this.name = data.name
+    this.completed_at = postedTime(data.completed_at);
+  }
+
+   addTaskToAssignment(){
+     return `<li><p>${task.name}</p><b>${this.completed_at}</b></li>`
+   }
+
+}
+
+ function showAllTasks(path) {
+   $.getJSON(path.success)(function(data) {
+     const tasks = data.map( taskJSON => {
+       const taskObj = new Task(taskJSON)
+       return taskObj.addTaskToAssignment()
+     })
+     $(#task_name).val("")
+     $('.taskAssignment').html(`<ul>${tasks.reverse().join('')}</ul>`)
+   })
+ }
+
+  function newTask(e) {
+    e.preventDefault()
+    const path = this.action
+    $.post(path, $(this).serialize(), function(data) {
+
+      showAllTasks(path)
+    })
+  }
+
+  function showTasks(e){
+    e.preventDefault()
+    showAllTasks(this.href)
+  }
+
+  function postedTime(completed_at){
+    date = new Date(completed_at)
+    return date.toLocaleString()
+  }
