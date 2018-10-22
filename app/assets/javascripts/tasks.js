b69f9c2165b19ed4e878d7a1adff52c1153387bb
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-10-21 19:20:58 -0700

    Remove ES6 class and move to assignment.js

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,18 +1,18 @@
-// class Task {
-//   constructor(data) {
-//     this.userName = data.user.name
-//     this.assignmentId = data.assignment_id
-//     this.userId = data.user_id
-//     this.name = data.name
-//     this.completed_at = postedTime(data.completed_at);
-//   }
+class Task {
+  constructor(data) {
+    this.userName = data.user.name
+    this.assignmentId = data.assignment_id
+    this.userId = data.user_id
+    this.name = data.name
+    this.completed_at = postedTime(data.completed_at);
+  }
 
-//    addTaskToAssignment(){
-//      return `<li><p>${task.name}</p><b>${this.completed_at}</b></li>`
-//    }
-//
-// }
-//
+   addTaskToAssignment(){
+     return `<li><p>${task.name}</p><b>${this.completed_at}</b></li>`
+   }
+
+}
+
 //  function showAllTasks(path) {
 //    $.getJSON(path.success)(function(data) {
 //      const tasks = data.map( taskJSON => {
@@ -24,14 +24,14 @@
 //    })
 //  }
 //
-//   function newTask(e) {
-//     e.preventDefault()
-//     const path = this.action
-//     $.post(path, $(this).serialize(), function(data) {
-//
-//       showAllTasks(path)
-//     })
-//   }
+  function newTask(e) {
+    e.preventDefault()
+    const path = this.action
+    $.post(path, $(this).serialize(), function(data) {
+
+      showAllTasks(path)
+    })
+  }
 //
 //   function showTasks(e){
 //     e.preventDefault()
