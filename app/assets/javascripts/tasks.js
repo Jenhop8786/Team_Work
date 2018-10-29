0acbbfc9fb0497b2092bcd8ff2880b56614419c9
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-10-28 19:05:57 -0700

    commented out class method for task

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,16 +1,36 @@
-// $(function() {
-//   $("#new_task").on("submit", function(event) {
-//       const data = $(this).serialize();  // serialize method creates a text string in standard URL encoded notation
-//       $.ajax({
-//           type: "POST",
-//           url: this.action,
-//           data: data,                         // form data
-//           success: function(response) {
-//               $("#task_name").val("");  // empty out text area
-//               let $ol = $("div.tasks ol")
-//               $ol.append(response);
-//           }
+// $(document).on('turbolinks:load', function() {
+//   attachListeners();
+// });
+//
+// const attachListeners = function() {
+//
+// // use Jquery event handler for whn link is clicked
+//   $("a.showTasks").on("click", function(e) {
+//     // Get Comments via jQuery AJAX .get Method
+//     $.get(this.href).success(function(json){
+//       // select the div comment container
+//       var $ol = $("div.comments ol")
+//       //empty div container so it doesn't reload list and double comments
+//       $ol.html("")
+//       // call the function for each comment in the array
+//       json.forEach(function(comment){
+//         $ol.append(`<li> <strong> <a href='/users/${comment.user.id}'> ${comment.user.email} </a></strong>:${comment.content}</li>`);
 //       })
-//       event.preventDefault();
+//     })
+//     e.preventDefault();
 //   })
 // })
+//
+// $(function() {
+//   function Comment(data) {
+//     this.id = data.id;
+//     this.content = data.content;
+//     this.user= data.user.email;
+//   }
+//
+//     Comment.prototype.formatComment = function() {
+//     var html = "<li> <strong> <a href='/users/"+ this.id + "'>" + this.user + "</a></strong>: "+ this.content + "</li>";
+//     return html;
+//   }
+//
+// }
